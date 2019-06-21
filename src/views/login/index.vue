8ab31c11e13ea75a7ef0ff6481eb1adf23859ced
fall66in <fall66in@163.com>
2019-06-21 22:27:31 +0800

    忽略git文件及用户登录-发送验证码-使用极验API初始化验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -28,6 +28,7 @@
 
 <script>
 import axios from 'axios'
+import '@/vendor/gt' // 引入极验 JavaScript SDK 文件
 export default {
   name: 'AppLogin',
   data () {
@@ -49,7 +50,33 @@ export default {
         method: 'GET',
         url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
-        console.log(res.data)
+        // 获取后端数据的data
+        const data = res.data.data
+        // console.log(res.data)
+        window.initGeetest(
+          {
+            // 以下配置参数来自服务端 SDK
+            gt: data.gt,
+            challenge: data.challenge,
+            offline: !data.success,
+            new_captcha: data.new_captcha,
+            product: 'bind' // 隐藏，直接弹出式
+          },
+          function (captchaObj) {
+            // console.log(captchaObj)
+            captchaObj
+              .onReady(function () {
+                captchaObj.verify() // 弹出验证码文本框
+              })
+              .onSuccess(function () {
+                // 极验得出的结果
+                console.log(captchaObj.getValidate())
+              })
+              .onError(function () {})
+
+            // 在这里注册按钮的点击事件，然后进行验证用户是否输入手机号以及手机号格式是否正确
+          }
+        )
       })
     }
   }
