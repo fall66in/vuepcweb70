f803df77c1bc8f8ee7ce31eed7037094d9c1955a
fall66in <fall66in@163.com>
2019-06-23 17:57:07 +0800

    表单验证-用户协议和验证手机号码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,11 +5,22 @@
       <div class="form-head">
         <img src="./logo_index.png" alt="黑马头条号">
       </div>
-      <el-form ref="form" class="form-content" :model="form">
-        <el-form-item>
+      <!-- 配置校验规则
+        rules 规则对象配置到el-form上,rules中配置的校验字段必须和表单数据一致
+        prop  检验字段配置到el-form-item上
+      JavaScript 触发验证
+        给el-form添加ref
+        调用this.$refs['ref名字'].validate(valid => {}) 触发验证 -->
+      <el-form
+      ref="form"
+      class="form-content"
+      :model="form"
+      :rules="rules"
+      >
+        <el-form-item prop="mobile">
           <el-input id="mobile" v-model="form.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- el-col 栅格布局，一共24列； :span 用来指定占用的大小， :offect 用来指定偏移量 -->
           <el-col :span="14">
             <el-input v-model="form.code" id="codes" placeholder="请输入验证码"></el-input>
@@ -17,6 +28,10 @@
           <el-col :offset="1" :span="9">
             <el-button @click="handleSendCode">获取验证码</el-button>
           </el-col>
+        </el-form-item>
+        <el-form-item prop="agree">
+          <el-checkbox class="agree-checkbox" v-model="form.agree"></el-checkbox>
+          <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></span>
         </el-form-item>
         <el-form-item>
           <el-button class="btn-login" id="code" type="primary" @click="handleLogin">登录</el-button>
@@ -29,20 +44,46 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt' // 引入极验 JavaScript SDK 文件
-import { setInterval, clearInterval } from 'timers'
 export default {
   name: 'AppLogin',
   data () {
     return {
       form: {
         mobile: '',
-        code: ''
+        code: '',
+        agree: ''
+      },
+      // form与rules的数据名字无关系，但是他们之间有关系
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // { len: 11, message: '长度必须为11个字符', trigger: 'blur' }
+          { pattern: /\d{11}/, message: '请输入有效的手机号', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          // { len: 6, message: '长度必须为6字符', trigger: 'blur' }
+          { pattern: /\d{6}/, message: '请输入有效的验证码', trigger: 'blur' }
+        ],
+        agree: [
+          { required: true, message: '请同意用户协议' },
+          { pattern: /true/, message: '请同意用户协议' }
+        ]
       }
     }
   },
   methods: {
     handleLogin () {
-      const { mobile, code } = this.form
+      // 使用form组件的validate方法触发校验，获取校验的结果状态，对整个表单进行校验
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        // 表单验证通过，提交登录请求
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -67,82 +108,66 @@ export default {
         })
     },
     handleSendCode () {
-      // console.log(1)
-      var mobile = document.querySelector('#mobile')
-      var str = mobile.value
-      var reg = /^\d{11}$/
-      if (str === '' || reg.test(str) === false) {
-        this.$message({
-          message: '请输入正确的手机号或者请输入手机号',
-          type: 'warning'
-        })
-      } else {
-        const { mobile } = this.form
-        axios({
-          method: 'GET',
-          url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
-        }).then(res => {
-          // 获取后端数据的data
-          const data = res.data.data
-          // console.log(res.data)
-          window.initGeetest(
-            {
-              // 以下配置参数来自服务端 SDK
-              gt: data.gt,
-              challenge: data.challenge,
-              offline: !data.success,
-              new_captcha: data.new_captcha,
-              product: 'bind' // 隐藏，直接弹出式
-            },
-            function (captchaObj) {
-              // console.log(captchaObj)
-              captchaObj
-                .onReady(function () {
-                  captchaObj.verify() // 弹出验证码文本框
-                  console.log(captchaObj.verify())
+      // 对部分表单字段--手机号是否有效 进行校验
+      this.$refs['form'].validateField('mobile', errorMessage => {
+        // console.log('errorMessage => ', errorMessage)
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        // 初始化验证码--验证通过
+        this.showGeetest()
+      })
+    },
+    // 初始化验证码
+    showGeetest () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        // 获取后端数据的data
+        const data = res.data.data
+        // console.log(res.data)
+        window.initGeetest(
+          {
+            // 以下配置参数来自服务端 SDK
+            gt: data.gt,
+            challenge: data.challenge,
+            offline: !data.success,
+            new_captcha: data.new_captcha,
+            product: 'bind' // 隐藏，直接弹出式
+          },
+          function (captchaObj) {
+          // console.log(captchaObj)
+            captchaObj
+              .onReady(function () {
+                captchaObj.verify() // 弹出验证码文本框
+                console.log(captchaObj.verify())
+              })
+              .onSuccess(function () {
+                // 极验得出的结果
+                const {
+                  geetest_challenge: challenge,
+                  geetest_seccode: seccode,
+                  geetest_validate: validate
+                } = captchaObj.getValidate()
+                axios({
+                  method: 'GET',
+                  url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+                  params: {
+                    challenge,
+                    validate,
+                    seccode
+                  }
+                }).then(res => {
+                  console.log(res.data)
+                  // 开启倒计时
                 })
-                .onSuccess(function () {
-                  // 极验得出的结果
-                  // console.log(captchaObj.getValidate())
-                  const {
-                    geetest_challenge: challenge,
-                    geetest_seccode: seccode,
-                    geetest_validate: validate
-                  } = captchaObj.getValidate()
-                  axios({
-                    method: 'GET',
-                    url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
-                    params: {
-                      challenge,
-                      validate,
-                      seccode
-                    }
-                  }).then(res => {
-                    console.log(res.data)
-                    // 开启倒计时
-                    var code = document.querySelector('#code')
-                    var codes = document.querySelector('#codes')
-                    var time = 60
-                    if (res.data.message === 'ok') {
-                      code.disabled = true
-                      var timer = setInterval(function () {
-                        if (time === 0 || codes.value !== 0) {
-                          clearInterval(timer)
-                          code.disabled = false
-                          code.value = '获取验证码'
-                        } else {
-                          code.value = time + '秒'
-                          time--
-                        }
-                      })
-                    }
-                  })
-                })
-                .onError(function () {})
-            } // 在这里注册按钮的点击事件，然后进行验证用户是否输入手机号以及手机号格式是否正确
-          )
-        })
-      }
+              })
+              .onError(function () {})
+          } // 在这里注册按钮的点击事件，然后进行验证用户是否输入手机号以及手机号格式是否正确
+        )
+      })
     }
   }
 }
@@ -159,20 +184,23 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
     img {
       width: 200px;
     }
   }
   .form-wrap {
     width: 350px;
-    height: 250px;
+    height: 290px;
     background-color: #fff;
     padding: 20px;
     border-radius: 10px;
     .btn-login {
       width: 100%;
     }
+  }
+  .agree-checkbox {
+    margin-right:5px;
   }
 }
 </style>
