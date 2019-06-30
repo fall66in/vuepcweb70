2f666fab357bfac1d9580a33b5be6b67f9d3fb90
fall66in <fall66in@163.com>
2019-06-30 22:10:04 +0800

    获取图片素材

--- src/views/source/index.vue
+++ src/views/source/index.vue
@@ -11,12 +11,12 @@
        <el-button type="primary">上传图片</el-button>
      </div>
      <el-row :gutter="20">
-       <el-col :span="4" v-for="(o) in 6" :key="o" style="margin-bottom: 10px">
+       <el-col :span="6" v-for="item in images" :key="item.id" style="margin-bottom: 10px">
         <el-card :body-style="{ padding: '0px' }">
-          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+          <img src="item.url" class="image" style="max-width: 100%">
           <div style="padding: 10px;">
             <div class="bottom clearfix">
-              <el-button type="primary" plain class="button" icon="el-icon-star-off" circle></el-button>
+              <el-button type="primary" plain class="button" :icon="item.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'" circle ></el-button>
               <el-button type="danger" icon="el-icon-delete" plain circle class="button" ></el-button>
             </div>
           </div>
@@ -31,7 +31,31 @@ export default {
   name: 'AppSource',
   data () {
     return {
-      active: '全部'
+      active: '全部',
+      images: []
+    }
+  },
+  created () {
+    this.loadImage()
+  },
+  methods: {
+    async loadImage (collect = false) {
+      try {
+        const data = await this.$http({
+          methods: 'GET',
+          url: '/user/images',
+          params: {
+            collect, // 是否查询收藏图片，默认查询所有
+            page: 1,
+            per_page: 10
+          }
+        })
+        console.log(data)
+        this.images = data.results
+      } catch (err) {
+        console.log(err)
+        this.$message.error('加载图片素材失败')
+      }
     }
   }
 }
